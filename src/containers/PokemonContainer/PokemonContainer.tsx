debef70092c609b2324cccc0e264ffc24efa94a2
kpogodin <gadzillllla1@gmail.com>
2022-04-26 20:46:41 +0400

    return neitral index

--- src/containers/PokemonContainer/PokemonContainer.tsx
+++ src/containers/PokemonContainer/PokemonContainer.tsx
@@ -1,41 +1,22 @@
-import Button from 'components/Button';
-import PokemonsList from 'components/PokemonsList';
-import { useEffect, useRef, useState } from 'react';
-import { useLazyGetListOfPokemonsQuery } from 'services/api/pokemon.api';
+// import Button from 'components/Button';
+// import PokemonsList from 'components/PokemonsList';
+// import { useLazyGetListOfPokemonsQuery } from 'services/api/pokemon.api';
 
 import s from './PokemonContainer.module.css';
 
 const PokemonContainer = () => {
-  const [loadPokemons, { data, error, isLoading }] = useLazyGetListOfPokemonsQuery();
-  const [currentRgb, setCurrentRgb] = useState('rgb(255,255,255)');
-  const [count, setCount] = useState(0);
-
-  const countRef = useRef(count);
-  countRef.current = count;
-
-  useEffect(() => {
-    const getRandomInt = (): any => Math.random() * 255;
-    const intervalId = setInterval(() => {
-      setCurrentRgb(`rgb(${getRandomInt()},${getRandomInt()},${getRandomInt()})`);
-      setCount(countRef.current + 5);
-    }, 200);
-    return () => clearInterval(intervalId);
-  }, [setCount, setCurrentRgb]);
+  // const [loadPokemons, { data, error, isLoading }] = useLazyGetListOfPokemonsQuery();
 
   return (
-    <div className={s.root} style={{ backgroundColor: currentRgb }}>
-      <h1 className={s.title} style={{ transform: `rotate(${-count}deg)` }}>
-        В МАЛАФЬЕ!
-      </h1>
-
-      <h1 className={s.semen}>ГАШНЯ!</h1>
-      <div className={s.hidden}>
+    <div className={s.root}>
+      <h1 className={s.title}>KPOGODIN</h1>
+      {/* <div>
         <Button onClick={() => loadPokemons()}>Загрузить покемонов из api/v1</Button>
         <br />
         {isLoading && <span data-testid="loader">Загрузка...</span>}
         {error && 'Ошибка'}
         {data && <PokemonsList data={data} />}
-      </div>
+      </div> */}
     </div>
   );
 };
